90af283dd6aaa873964b351c8628e9e8799d426d
elys33ve <elys33ve@gmail.com>
2021-12-12 17:36:45 -0500

    arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,10 @@
+#### Arithmetic Operators: mathy shits                                                      
+2 + 2            #addition
+3 - 2            #subtraction
+2 * 3            #multiplication
+3/2              #division
+3**2             #exponents
+3//2             #floor division (rounds down to nearest whole #)
+1 % 2            #modulo (division where it outputs the remainder. (18%12 would output 6))
+(1 + 1)/2 + 1    #PEMDAS so parentheses when needed
+1_000_000_000    #underscores make big #s more readable and dont affect value
