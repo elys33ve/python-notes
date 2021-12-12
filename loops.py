9c4c34b85f2c8a0bbcadf2829e73c80f243dd659
elys33ve <elys33ve@gmail.com>
2021-12-12 18:53:00 -0500

    loops.py

---
+++ loops.py
@@ -0,0 +1,26 @@
+#### For Loops:                         (for iterate specific # of times)
+List = ['a','b','c','d','e','f'] #list name can be any variable btw
+                                                              
+for i in range(0,5):        #for loop w range              
+  print("hi")                   #executes for each value in range
+print(i)                    #iterations (doesnt have to b 'i' just whatever variable u have in that position)
+
+for List in List:           #for loop w list 
+  print(List)                   #executes each item in list
+ 
+
+#### While Loops:                       (# of iterations determined by if condition is met)
+T = True
+x = 1
+
+while T == True:          #while loop                                                   ---> set up like:       while *condition*:
+  x = x + 8                   #performs as long as condition True
+  if x > 100:
+    print(x)          
+    T = False                 #makes condition False, stops while loop when if condition me
+  elif x > 200:
+    break                     #break will break loop
+  else:
+    pass
+
+  
