7d5ebfe9fb11c491a237ef175c51eec8af0c34b5
elys33ve <elys33ve@gmail.com>
2021-12-12 18:29:31 -0500

    operators.py

--- operators.py
+++ operators.py
@@ -8,3 +8,40 @@
 1 % 2            #modulo (division where it outputs the remainder. (18%12 would output 6))
 (1 + 1)/2 + 1    #PEMDAS so parentheses when needed
 1_000_000_000    #underscores make big #s more readable and dont affect value
+
+#### Comparison Operators:
+1==1      #equal
+1!=2      #not equal
+2>1       #greater than
+2<1       #less than
+1>=2      #greater or equal
+1<=2      #less or equal
+
+#### Logical & Identity Operators:
+x>1 and y<3                   #returns True if both statements are True
+x>1 or y<3                    #returns True if one or both statements are True
+not(1==1)                     #returns True if the stuff inside parenthases is False
+
+#### Membership:
+x in l              #returns True if value of x is in sequence l
+x not in l
+
+#### Identity:
+x is y              #returns True if value of x = y
+x is not y
+
+#### Assignment:
+x = 5                   
+x += 5        #same as x = x + 5
+x -= 5
+x *= 5
+x /= 5
+x //= 5
+x %= 5
+x **= 5
+x &= 5
+x |= 5
+x ^= 5
+x >>= 5
+x <<= 5
+
