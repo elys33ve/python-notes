1ce64cddac076d3d97e3f5c2dc85c39c1300a9a5
elys33ve <elys33ve@gmail.com>
2021-12-12 18:57:07 -0500

    functions.py

---
+++ functions.py
@@ -0,0 +1,13 @@
+#### Funtions:
+x = 1
+y = 2
+z = 3
+                                                                                                                      
+def this():           #defining a function (parameters/arguments in parenthesis)
+  global z                      #use global to modify a global variable from inside function
+  global x
+  x = x * y                     #what the function does
+  z = z * y                            
+  return x, z                   #use 'return' to tell it what to output/where to go next
+
+this()                #calling the function
