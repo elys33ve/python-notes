3fbbe889205b4a79607705f7aed4229d95dd800b
elys33ve <elys33ve@gmail.com>
2021-12-15 04:19:28 -0500

    conditionals.py

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -3,6 +3,8 @@ x = 1
 y = 2 
 z = 3
 
+#if, elif, else
+                    #ex. 1
 if x < y:                              #if statement
   print("x is less than y")                #executes if (x<y in this case) is TRUE
 elif x > y:                            #elif statement ("else if")
@@ -12,6 +14,7 @@ elif x == y:                           #(u can have many elifs)
 else:                                  #else statement
   print("huh?")                            #executes if all of the elif or if statements are FALSE 
 
+                    #ex. 2
 if x + y == z:
   print("x plus y equals z")
   if x and y == 1 or 2:                  #nested if statement
