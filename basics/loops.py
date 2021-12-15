951d0b206ca8637a93d221f1259888875e07585d
elys33ve <elys33ve@gmail.com>
2021-12-15 04:40:13 -0500

    loops.py

--- basics/loops.py
+++ basics/loops.py
@@ -1,6 +1,8 @@
-#### For Loops:                         (for iterate specific # of times)
+##### LOOPS #####                        
+
+#for loops --> (for iterate specific # of times)
 List = ['a','b','c','d','e','f'] #list name can be any variable btw
-                                                              
+
 for i in range(0,5):        #for loop w range              
   print("hi")                   #executes for each value in range
 print(i)                    #iterations (doesnt have to b 'i' just whatever variable u have in that position)
@@ -9,7 +11,7 @@ for List in List:           #for loop w list
   print(List)                   #executes each item in list
  
 
-#### While Loops:                       (# of iterations determined by if condition is met)
+#while loops --> (# of iterations determined by if condition is met)
 T = True
 x = 1
 
