8264a0185fdbe665203bc6b6f46b5cafe691a6ea
elys33ve <elys33ve@gmail.com>
2021-12-15 04:34:27 -0500

    files.py

--- basics/files.py
+++ basics/files.py
@@ -1,12 +1,15 @@
-#### Reading/Writing to Files:
+##### FILES #####
+
 filename = "text.txt"                 #if same directory
 filename = "C:\\files\text.txt"       #if not in same dir (add path)
 
+#opening files                                  ('t' = text (defult), 'b' = binary. --> 'rt' or 'rb' for 'r')
 read = open(filename, 'r')            #read file
 app = open(filename, 'a')             #append/add to file
 write = open(filename, 'w')           #write/write over file
 rewr = open(filename, 'w+')           #read and write file
 
+#read/write files
 print(read.read(10))                  #read (# of characters)
 app.write("stuff")                    #append (string of stuff)
 write.write("stuff")                  #write (string of stuff)
