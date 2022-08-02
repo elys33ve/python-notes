6939db89f0d95ccc7c8c197d6d15cbc9d3377932
elys33ve <elys33ve@gmail.com>
2022-08-02 12:27:32 -0400

    files.py

--- basics/files.py
+++ basics/files.py
@@ -1,19 +1,20 @@
 ##### FILES #####
 
-filename = "text.txt"                 #if same directory
-filename = "C:\\files\text.txt"       #if not in same dir (add path)
+filename = "text.txt"                 # if same directory
+filename = "C:\\files\text.txt"       # if not in same dir (add path)
+filename = "/home/path/text.txt"      # (linux)
 
 #opening files                                  ('t' = text (defult), 'b' = binary. --> 'rt' or 'rb' for 'r')
-read = open(filename, 'r')            #read file
-app = open(filename, 'a')             #append/add to file
-write = open(filename, 'w')           #write/write over file
-rewr = open(filename, 'w+')           #read and write file
+read = open(filename, 'r')            # read file
+app = open(filename, 'a')             # append/add to file
+write = open(filename, 'w')           # write/write over file
+rewr = open(filename, 'w+')           # read and write file
 
 #read/write files
-print(read.read(10))                  #read (# of characters)
-app.write("stuff")                    #append (string of stuff)
-write.write("stuff")                  #write (string of stuff)
-read.close()                          #close file
+print(read.read(10))                  # read (# of characters)
+app.write("stuff")                    # append (string of stuff)
+write.write("stuff")                  # write (string of stuff)
+read.close()                          # close file
 
-for x in read:                        #prints line by line
+for x in read:                        # prints line by line
   print(x)
